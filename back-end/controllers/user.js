66e6cecaee7b82bb4f02473e4433478feef8a1a0
Akash <akash.puranik06@gmail.com>
2024-12-02 23:02:11 +0530

    password hashing done

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -1,23 +1,32 @@
 const User = require('../models/user');
+const bcrypt = require("bcrypt");
 
 exports.postUserSignUp = async (req, res) => {
     const { name, email, phone, password } = req.body;
-    console.log(name,'name',email,'email',phone,'phone',password);
+    console.log(name, 'name', email, 'email', phone, 'phone', password);
     try {
-        console.log('Incoming request body:', req.body);
         const existingUser = await User.findOne({ where: { email } });
 
         if (existingUser) {
-            console.log('in existing user',name,'name',email,'email',phone,'phone',password);
+            console.log('in existing user', name, 'name', email, 'email', phone, 'phone', password);
 
             return res.status(400).json({ error: 'User already exists' });
-            
-        }
-        console.log('in outer user',name,'name',email,'email',phone,'phone',password);
 
-        const newUser = await User.create({ name, email, phone, password });
+        }
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user
+        const newUser = await User.create({
+            name,
+            email,
+            phone,
+            password: hashedPassword,
+        });
+
         res.status(201).json({ message: 'User added successfully', user: newUser });
-    } catch (err) {
+    }
+    catch (err) {
         console.error("Error in POST /add-user:", err);
         res.status(500).json({ error: 'An error occurred while adding the user.' });
     }
@@ -28,20 +37,29 @@ exports.postUserSignUp = async (req, res) => {
 exports.postUserLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-  
-    User.findOne({ where: { email: email } }).then((user) => {
-      if (user) {
-        if (user.password == password) {
-          
-          res.status(200).json({ message: 'Login successfully', user: user });
 
-        } else {
-             res.status(401).json({ error: 'Wrong Password' });
+    User.findOne({ where: { email: email } })
+        .then((user) => {
+            if (user) {
 
-        }
-      } else {
-        return res.status(404).json({ error: 'User not found' });
+                bcrypt.compare(password, us.password, async (err, result) => {
 
-      }
-    });
-  };
+                    if (err) {
+                        return res.status(500).json({ error: 'something went  wrong' });
+                    }
+
+                    if (result === true) {
+                        res.status(200).json({ message: 'Login successfully', user: user });
+
+                    }
+                    else {
+                        res.status(401).json({ error: 'Wrong Password' });
+                    }
+                });
+
+            } else {
+                return res.status(404).json({ error: 'User not found' });
+
+            }
+        });
+};
